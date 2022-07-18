f22e7e4dede688c9565065eac5040d43cbb90d8a
David Alexandre Fernandes <davidalexandrefernandes@outlook.com>
2022-07-18 04:17:30 -0300

    fix: form http

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -97,7 +97,7 @@ const Contact = () => {
             <div className='p-4'>
               <form
                 onSubmit={handleSubmit}
-                action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
+                action='https://'
                 method='POST'
               >
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
